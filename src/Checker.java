b0febb33753fcf366dccd2d8e64aa802febfe4dc
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-03-14 13:48:38 -0400

    Add files via upload

--- src/Checker.java
+++ src/Checker.java
@@ -8,47 +8,47 @@ public class Checker {
         this.userInput = "";
         this.arraylength1 = 0;
         this.arraylength2 = 0;
-        this.score = 100;
+        this.score = 0;
     }
     public void FirstChar(String firstWord, int length) {
         if (firstWord.equals("")){
-            score=0;             //empty word sentence zero point
+            score=100;             //empty word sentence zero point
         }
         else if (firstWord.equals(" ")){
-            score-=10;      //first char is " ",-10 points
+            score+=10;      //first char is " ",-10 points
         }
         else {
             char firstChar = firstWord.charAt(0);
             if (!Character.isUpperCase(firstChar)){
-                score-=10;   //did not capitalize, -10 points
+                score+=10;   //did not capitalize, -10 points
             }
         }
     }
 
     public void LastChar(String lastWord) {
         if (!lastWord.endsWith("?") && !lastWord.endsWith("!") && !lastWord.endsWith(".")){
-            score-=20;  //no proper punctuation,-20
+            score+=20;  //no proper punctuation,-20
         }
     }
 
     public void CheckLength(int length) {
         if (length >= 35) {
-            score-=20; //sentence too long(over 35 words),-20 points
+            score+=20; //sentence too long(over 35 words),-20 points
         }
     }
 
     public void WeakWord(String reallyVery, int length) {
         if (reallyVery.equalsIgnoreCase("very")) {
-            score-=5;  //use weak word "very",-5
+            score+=5;  //use weak word "very",-5
         }
         else if (reallyVery.equalsIgnoreCase("really")){
-            score-=5;  //use weak word "really",-5
+            score+=5;  //use weak word "really",-5
         }
     }
 
     public void CheckNums(String wordNum) {
         if (wordNum.equalsIgnoreCase("0") || wordNum.equalsIgnoreCase("1") || wordNum.equalsIgnoreCase("2") || wordNum.equalsIgnoreCase("3") || wordNum.equalsIgnoreCase("4") || wordNum.equalsIgnoreCase("5") || wordNum.equalsIgnoreCase("6") || wordNum.equalsIgnoreCase("7") || wordNum.equalsIgnoreCase("8") || wordNum.equalsIgnoreCase("9")){
-            score-=5; //did not spell numbers,-5
+            score+=5; //did not spell numbers,-5
         }
     }
 
@@ -56,15 +56,15 @@ public class Checker {
         if (quote.equalsIgnoreCase("\"")){
             quoteCount++;
         }
-        if (quoteCount % 2 != 0){//It's odd, which means there's a missing quote
-            score-=5;  //quote use error,-5
+        if (quoteCount % 2 != 0){
+            score+=5;  //quote use error,-5
         } //else its even, which is fine.
     }
 
     public void sameWord(Object[] userInArray) {
         for (int i = 1; i < userInArray.length; i++){
             if (( (String) userInArray[i]).equalsIgnoreCase((String) userInArray[i - 1])) {
-                score-=30; //repeat a word twice,-30
+                score+=30; //repeat a word twice,-30
             }
         }
     }
@@ -72,7 +72,7 @@ public class Checker {
     public void CheckSpace(String userInArray) {
         for (int i = 0; i < userInArray.length()-1; i++){
             if (userInArray.charAt(i)==' ' && userInArray.charAt(i)==userInArray.charAt(i+1)) {
-                score-=10; //two much space between two words,-10
+                score+=10; //two much space between two words,-10
             }
         }
     }
@@ -82,14 +82,14 @@ public class Checker {
             String aAfter = (String) (userInArray[i + 1]);
             String a2 = aAfter.substring(0, 1);
             if (a2.equalsIgnoreCase("a") || a2.equalsIgnoreCase("e") || a2.equalsIgnoreCase("i") || a2.equalsIgnoreCase("o") || a2.equalsIgnoreCase("u")){
-                score-=10; //should use "an",-10
+                score+=10; //should use "an",-10
             }
         }
         else if (aAn.equalsIgnoreCase("an")){ //a,e,i,o,u (vowel)
             String anAfter = (String) (userInArray[i + 1]);
             String an2 = anAfter.substring(0,1);
             if (!(an2.equalsIgnoreCase("a") || an2.equalsIgnoreCase("e") || an2.equalsIgnoreCase("i") || an2.equalsIgnoreCase("o") || an2.equalsIgnoreCase("u"))){
-                score-=10; //should use "a",-10
+                score+=10; //should use "a",-10
             }
         }
     }
@@ -104,10 +104,27 @@ public class Checker {
                 }
             }
             if (notAOBcount == userInArray.length){
-                score-=10; //should better add "and","but","or"
+                score+=10; //should better add "and","but","or"
             }
         }
     }
+    public Map<String,Integer> CheckPhrases(Map<String,Integer> phrases,String dirName)throws IOException{ //check whether the phrase is in the phrase list
+        for (String item:phrases.keySet()){
+            File phraseFile = new File(dirName + "commonPhrases.txt");
+            BufferedReader in = new BufferedReader(new FileReader(phraseFile));
+            String line;
+            while ((line = in.readLine()) != null){
+                item=item.replaceAll("[.,?!]","");
+                if (line.equalsIgnoreCase(item)){
+                    phrases.replace(item,0);
+                    break;
+                }
+            }
+            in.close();
+        }
+        return phrases;
+    }
+
     public void CheckNounVerb(Object[] userInArray, String dirName) throws IOException {
         int nounPlace = 0, verbPlace = 0;
         boolean nounFound = false, verbFound = false;
@@ -115,7 +132,7 @@ public class Checker {
         for (int i = 0; i < userInArray.length; i++){
             String currentNoun = (String) userInArray[i];
 
-            File nounFile = new File(dirName + "nounList.txt");//Assigns noun file
+            File nounFile = new File(dirName + "nounList.txt"); //assign the noun file
             BufferedReader in = new BufferedReader(new FileReader(nounFile));
             String line;
             while ((line = in.readLine()) != null){
@@ -144,17 +161,17 @@ public class Checker {
         }
         if ((nounFound) && (verbFound)){ //if both were found:
             if (nounPlace > verbPlace){//check if noun didn't come first
-                score-=20; // should have verb before a noun
+                score+=20; // should have verb before a noun
             }
         }
         else if (nounFound == false){//otherwise one is missing
-            score-=30; //no noun found
+            score+=30; //no noun found
             if (verbFound == false){
-                score-=30; //no verb found
+                score+=30; //no verb found
             }
         }
         else if (verbFound == false){//otherwise one is missing
-            score-=30; //no verb found
+            score+=30; //no verb found
         }
     }
     static Checker c1 = new Checker();
@@ -166,10 +183,15 @@ public class Checker {
         userInNoPunc = userInput.replaceAll("[^a-zA-Z\\s]", "").replaceAll("\\s+", " ");
 
         while (!userInput.equalsIgnoreCase("quit")){
+            Map<String,Integer> phrases=new HashMap<>();  //used to store different phrases of a sentence
             String[] userInArray = userInNoPunc.split(" "); //each word WITHOUT PUNCTUATION is an element
             String[] userInArray2 = userInput.split(""); //each character is an element
             String[] userInArray3 = userInput.split(" "); //each word is an element
             String userInArray4=userInput; //the raw input string
+            for (int i = 0, l = userInArray.length; i + 1 < l; i++)
+                phrases.put(userInArray[i] + " " + userInArray[i + 1], 100);  //add the phrases with length==2
+            for (int i = 0, l = userInArray.length; i + 3 < l; i++)
+                phrases.put(userInArray[i] + " " + userInArray[i + 1]+" "+userInArray[i+2],100); //add the phrases with length==3
             arraylength1 = userInArray.length;
             arraylength2 = userInArray2.length;
 
@@ -199,6 +221,8 @@ public class Checker {
             c1.CheckLength(arraylength1);
             c1.sameWord(userInArray3);
             c1.CheckSpace(userInArray4);
+            phrases=c1.CheckPhrases(phrases,dirName);  //check whether the phrase is in the commonPhrase list
+
             int quoteCount = 0;
             for (int i = 0; i < userInArray2.length; i++){
                 String quote = userInArray2[i];
@@ -214,8 +238,17 @@ public class Checker {
                 String aAn = userInArray[i];
                 c1.CheckAandAN(aAn, userInArray, i);
             }
+            int phraseAverage=0;
+            for (int value:phrases.values()){
+                phraseAverage+=value;
+            }
+            phraseAverage=phraseAverage/phrases.size();
+            score+=phraseAverage;
             System.out.println("the score of your sentence is:"+score);
-            score=100;   //update the score
+            System.out.println("the scores of the phrases is:");
+            for (Map.Entry<String,Integer> entry:phrases.entrySet())
+                System.out.println(entry.getKey()+":"+entry.getValue());
+            score=0;   //update the score
             System.out.println("----------------------------------------------------------");
             System.out.println("Please enter another sentence. Type 'quit' to exit.");
             userInput = sc.nextLine();
